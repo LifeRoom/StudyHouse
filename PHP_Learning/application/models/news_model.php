2ed2cf0d0211ddd4f8ad7cfb8cd468640991c4f4
kaitoukidd <zangrenlan@163.com>
2015-05-07 22:43:13 +0800

    PHP_Learning 0507

---
+++ PHP_Learning/application/models/news_model.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class news_model extends CI_Model{
+    
+    public function __construct(){
+        $this->load->database();
+    }
+    
+    public function get_news($slug = FALSE){
+        
+        if ($slug == FALSE){
+            $query=$this->db->get('news');
+            return $query->result_array();
+        } 
+        
+        $query = $this->db->get_where('news' ,array('slug' => $slug));
+        return $query->row_array();
+    }
+    
+    }
+
---
+++ PHP_Learning/application/views/templates/header.php
@@ -0,0 +1,6 @@
+<html>
+    <head>
+        <title><?php echo $title?>-CI2tutorial</title>
+    </head>
+    <body>
+        <h1>CI2 tutorial</h1>
